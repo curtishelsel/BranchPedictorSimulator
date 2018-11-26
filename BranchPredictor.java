872e26ea53ec60faa660d47a49de803d067a5dfa
Curtis Helsel <curtisdhelsel@knights.ucf.edu>
2018-11-26 16:03:06 -0500

    Set up intial variables

--- BranchPredictor.java
+++ BranchPredictor.java
@@ -1,16 +1,41 @@
+/* Curtis Helsel
+   EEL4768 - Fall 2018
+   November 26, 2018
 
+   This program simulates a global branch predictor.
+*/
 
 public class BranchPredictor{
-
-
-	public BranchPredictor(){
 	
+	private int m;
+	private int n;
+	private int[] table;
+
+	public BranchPredictor(int m, int n, String fileName){
+
+		this m = m;
+		this n = n;
+
+		
+
+		table = new int[Math.pow(2,m)];
 	}
 	
 	
-	public static void main(String args){
+	
+	private void printStatistics(){
 	
 	
+	}
+	
+	public static void main(String[] args){
+	
+		BranchPredictor bp = new BranchPredictor(
+				Integer.parseInt(args[0]),
+				Integer.parseInt(args[1]),
+				args[2]);
+	
+		bp.printStatistics();
 	}
 
 
